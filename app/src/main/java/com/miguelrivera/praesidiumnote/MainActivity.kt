71c7496dd1fb45ee7974a78f5b93556abfb67f74
Miguel Rivera <rivera.migueld@gmail.com>
2025-12-17 22:51:13 +0900

    feat: add PraesidiumApp.kt, which is a singleton component that lives for the entire lifecycle of the app feat: added 3 TrueTypeFont files for Inter, and created a new Typography inside Type.kt that the PraesidiumNotes will use feat: updated MainActivity.kt with @AndroidEntryPoint to mark it as injectable by hilt
    closes #3 and #4

--- app/src/main/java/com/miguelrivera/praesidiumnote/MainActivity.kt
+++ app/src/main/java/com/miguelrivera/praesidiumnote/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.miguelrivera.praesidiumnote.presentation.ui.theme.PraesidiumNoteTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/miguelrivera/praesidiumnote/PraesidiumApp.kt
@@ -0,0 +1,7 @@
+package com.miguelrivera.praesidiumnote
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PraesidiumApp: Application()
